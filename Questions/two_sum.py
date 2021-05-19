d640ff859f38925d34116b4714459187024c2dc0
Magnum-07 <66016411+Magnum-07@users.noreply.github.com>
2021-05-19 14:50:51 +0530

    Updated two_sum

--- Questions/two_sum.py
+++ Questions/two_sum.py
@@ -1,10 +1,12 @@
 A = [3, 5, 2, -4, 8, 11]
 target_sum = 7
 
-
+# Time Complexity(T.C.) = O(n)
+# Space Complexity(S.C.) = O(n)
 def two_sum(arr, target_value):
+#     To solve this question by using of hashing
     seen = dict()
-
+    
     for i, n in enumerate(arr):
         if n in seen:
             return [seen[n], i]
